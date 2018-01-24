097bb8e886dd88b80923c000466268e1eb90f602
Daniel Dadap <daniel@dadap.net>
2018-01-23 21:37:48 -0600

    Remove unused override functions
    This includes removing the code to change text color on the next keyboard
    button depending on keyboard theme.

--- pIqaD/KeyboardViewController.swift
+++ pIqaD/KeyboardViewController.swift
@@ -154,27 +154,4 @@ class KeyboardViewController: UIInputViewController {
     @IBAction func slideOut(sender: UIButton) {
         popIn(key: sender)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
-    }
-    
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
-    }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-    }
-
 }
